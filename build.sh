d14e2cf221fb3dae1e349c980d8a35ce3306535c
Vivek Kumar <vivek.ispat@gmail.com>
2019-12-20 04:31:11 -0800

    fix a few things

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ setup_config() {
 
   git remote set-url origin git@github.com:vivekkr12/travis-docker-git-ci-cd.git
   echo "$TRAVIS_DEPLOY_KEY" > "$HOME"/.ssh/id_rsa
-  echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > "$HOME"/.ssh/known_hosts
+  chmod 400 "$HOME"/.ssh/id_rsa
 
   # setup docker
   echo "$TRAVIS_DOCKER_PASSWORD" | docker login -u "$TRAVIS_DOCKER_USERNAME" --password-stdin
